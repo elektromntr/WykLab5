796060cfb37e397dd33d6c9327287b3a1eb63d4e
barto <barto@LENOVO-E530-BAR>
2017-05-10 20:13:32 +0200

    Add project files.

---
+++ WykLab5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WykLab5.Startup))]
+namespace WykLab5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
